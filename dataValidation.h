57bb078fc5360ad34e8c89081bc29802ed5cebba
ReinDC <124887037+ReinDC@users.noreply.github.com>
2023-08-02 02:47:58 +0800

    Add files via upload

--- dataValidation.h
+++ dataValidation.h
@@ -1,10 +1,12 @@
+#define MAX 999
+#define length 20
 
 typedef char string20[21];
 typedef char string1000[1001];
+typedef char string50[51];
 
+
+void password(char *password);
 int campreDate(int todayM, int todayD, int todayY, int futureM, int futureD, int futureY);
 int dateValid(int dateM, int dateD, int dateY);
-int stringValid(char *string);
-int yearValid(int year);
 int dayValid(char *day);
-
